52507d5df472995239d571c4894c9e7511e6b712
Muthller2 <105158409+Muthller2@users.noreply.github.com>
2022-05-29 10:57:10 -0400

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,5 @@
+api_hash = "a9cea9f075384f489aebb85168bb0e58"
+api_id = 16991146
+bot_token = "5305098621:AAHhwMAuzC4vh7ISyKboBXEZ2qrO0UJOnc4"
+authorized_users = ['SPDTHENINO_70','DavidT0T','Shiro111','Adam_jt','brea02']
+admin_user = 'Comillas_San'
