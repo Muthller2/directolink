2b96fefd3491f073a1eada77ce69a812fd174486
Muthller2 <105158409+Muthller2@users.noreply.github.com>
2022-05-29 11:25:58 -0400

    Add files via upload

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "a9cea9f075384f489aebb85168bb0e58"
 api_id = 16991146
-bot_token = "5305098621:AAHhwMAuzC4vh7ISyKboBXEZ2qrO0UJOnc4"
+bot_token = "5436809071:AAGPFYLaqXPTHuOz5jA2m0lGisFcRCpz1b4"
 authorized_users = ['SPDTHENINO_70','DavidT0T','Shiro111','Adam_jt','brea02']
 admin_user = 'Comillas_San'
