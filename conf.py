4046f59e14173a2a52b7d5fff9c05d657065ca0d
Muthller2 <105158409+Muthller2@users.noreply.github.com>
2022-07-18 03:06:02 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "a9cea9f075384f489aebb85168bb0e58"
 api_id = 16991146
-bot_token = "5436809071:AAGPFYLaqXPTHuOz5jA2m0lGisFcRCpz1b4"
-authorized_users = ['SPDTHENINO_70','DavidT0T','Shiro111','Adam_jt','brea02']
+bot_token = "5357525164:AAE56W6EZq26nh2lO4eASGJ1-qzNIAtjjKc"
+authorized_users = ['SPDTHENINO_70','DavidT0T','antronix2381','Shiro111','Adam_jt','brea02']
 admin_user = 'Comillas_San'
