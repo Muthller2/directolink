640f06f9fb0b9749dddadd3e852abe9bcd71fcfe
Muthller2 <105158409+Muthller2@users.noreply.github.com>
2022-07-30 00:48:35 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "a9cea9f075384f489aebb85168bb0e58"
 api_id = 16991146
-bot_token = "5357525164:AAE56W6EZq26nh2lO4eASGJ1-qzNIAtjjKc"
-authorized_users = ['SPDTHENINO_70','DavidT0T','antronix218','Shiro111','Adam_jt','brea02']
+bot_token = "5383280975:AAEcuDMjXB64BzslBF_gCw-j3Te63ryiL4E"
+authorized_users = ['Cris8296']
 admin_user = 'Comillas_San'
